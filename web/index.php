befc5fc6314dfe4683f70baf13e61e9b9c237e2e
xilesun <2013xile@gmail.com>
2017-01-23 12:17:39 +0800

    use hash and bindParam

--- web/index.php
+++ web/index.php
@@ -29,24 +29,34 @@ $route->post('/signin', function(){
   $conn = getPDO();
   $user = $_POST['user'];
   $pass = $_POST['pass'];
-  $num = $conn->prepare("SELECT id FROM user WHERE username = '$user' AND password = '$pass'");
-  $num->execute();
+
+  $sql = $conn->prepare("SELECT password FROM user WHERE username = :user");
+  $sql->bindParam(':user', $user, PDO::PARAM_STR);
+  $sql->execute();
   
-  if($num->rowCount() == 1) {
-    $_SESSION['user'] = $user;
-    header('location: /');
+  if($sql->rowCount() == 1) {
+    $hash = $sql->fetchAll();
+    if(password_verify($pass, $hash[0]['password'])) {
+      $_SESSION['user'] = $user;
+      header('location: /');
+    } else {
+      echo 'Password incorrect';
+    }
   } else {
-    header('location: /signin');
+    echo 'Username incorrect';
   }
 });
 
 $route->post('/signup', function(){
   $conn = getPDO();
   $user = $_POST['user'];
-  $pass = $_POST['pass'];
+  $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
   $email = $_POST['email'];
-
-  $sql = $conn->prepare("INSERT INTO `user`(`username`, `password`, `email`) VALUES ('$user', '$pass', '$email')");
+  echo $user . $pass . $email;
+  $sql = $conn->prepare("INSERT INTO `user`(`username`, `password`, `email`) VALUES (:user, :pass, :email)");
+  $sql->bindParam(':user', $user, PDO::PARAM_STR);
+  $sql->bindParam(':pass', $pass, PDO::PARAM_STR);
+  $sql->bindParam(':email', $email, PDO::PARAM_STR);
   if($sql->execute()){
       header("location: /signin");
   }
@@ -56,7 +66,8 @@ $route->get('/validateuser', function(){
   $conn = getPDO();
   $user = $_GET['user'];
   //check whether the username exists or not
-  $check = $conn->prepare("SELECT id FROM user WHERE username = '$user'");
+  $check = $conn->prepare("SELECT id FROM user WHERE username = :user");
+  $check->bindParam(':user', $user);
   $check->execute();
 
   if($check->rowCount() > 0) {
