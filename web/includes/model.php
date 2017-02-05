62abceb59d86d50bb27859b019b3e43215c2e806
xilesun <2013xile@gmail.com>
2017-02-05 17:03:58 +0800

    abstract class

---
+++ web/includes/model.php
@@ -0,0 +1,61 @@
+<?
+include('conn.php');
+
+class db_model {
+  protected $table_name; //defined in individual object
+  protected $prop = array();
+
+  public function findByUser($value, $user) {
+    global $conn;
+
+    $sql = "SELECT " . $value . " FROM " . $this->table_name . " WHERE username = :user";
+
+    $q = $conn->prepare($sql);
+    $q->bindParam(':user', $user, PDO::PARAM_STR);
+    $q->execute();
+
+    if($q->rowCount() > 0) {
+      $result = $q->fetch();
+      return $result;
+    } else {
+      return false;
+    }
+  }
+
+  public function __set($key, $value) {
+    return $this->prop[$key] = $value;
+  }
+
+  public function __get($key) {
+    return $this->prop[$key];
+  }
+
+  public function save() {
+    global $conn;
+
+    $sql_key = "";
+    $sql_value = "";
+    $n = 0;
+    $count = count($this->prop);
+
+    foreach($this->prop as $key => $value){
+      $n++;
+      $sql_key .= "`" . $key . "`";
+      $sql_value .= ":" . $key;
+      if($n != $count) {
+        $sql_key .= ", "; 
+        $sql_value .= ", ";
+      }
+    }
+
+    $sql = "INSERT INTO `" . $this->table_name . "`(" . $sql_key . ") VALUES (" . $sql_value . ")";
+
+    $q = $conn->prepare($sql);
+
+    foreach($this->prop as $key => $value){
+      $bind_var = ':' . $key;
+      $q->bindParam($bind_var, $value, PDO::PARAM_STR);
+    }
+    $q->execute();
+  }
+}
--- web/index.php
+++ web/index.php
@@ -1,16 +1,19 @@
 <?php
 session_start();
-include('includes/conn.php');
+include('includes/model.php');
 require 'vendor/autoload.php';
 
 use Phroute\Phroute\RouteCollector;
 use Phroute\Phroute\Dispatcher;
-use twig\twig;
 
 $route = new RouteCollector();
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
+
+class User extends db_model {
+  protected $table_name = 'user';
+}
 
 $route->get('/', function() use ($twig) {
   if(isset($_COOKIE['user'])) {
@@ -38,16 +41,15 @@ $route->post('/signin', function(){
 
   global $conn;
 
+  $new_user = new User();
+
   $user = $_POST['user'];
   $pass = $_POST['pass'];
 
-  $sql = $conn->prepare("SELECT password FROM user WHERE username = :user");
-  $sql->bindParam(':user', $user, PDO::PARAM_STR);
-  $sql->execute();
-  
-  if($sql->rowCount() == 1) {
-    $hash = $sql->fetch();
-    if(password_verify($pass, $hash['password'])) {
+  $result = $new_user->findByUser('password', $user);
+
+  if($result) {
+    if(password_verify($pass, $result['password'])) {
       $_SESSION['user'] = $user;
       if($_POST['remember'] == 'on') {
         setcookie('user', $user, time()+60*60*24*7);
@@ -64,29 +66,30 @@ $route->post('/signin', function(){
 $route->post('/signup', function(){
   global $conn;
 
+  $new_user = new User();
+
   $user = $_POST['user'];
   $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
   $email = $_POST['email'];
 
-  $sql = $conn->prepare("INSERT INTO `user`(`username`, `password`, `email`) VALUES (:user, :pass, :email)");
-  $sql->bindParam(':user', $user, PDO::PARAM_STR);
-  $sql->bindParam(':pass', $pass, PDO::PARAM_STR);
-  $sql->bindParam(':email', $email, PDO::PARAM_STR);
-  if($sql->execute()){
-      header("location: /signin");
-  }
+  $new_user->username = $user;
+  $new_user->password = $pass;
+  $new_user->email = $email;
+
+  $new_user->save();
+  # header("location: /signin");
 });
 
 $route->get('/validateuser', function(){  
   global $conn;
   
+  $new_user = new User();
+
   $user = $_GET['user'];
   //check whether the username exists or not
-  $check = $conn->prepare("SELECT id FROM user WHERE username = :user");
-  $check->bindParam(':user', $user);
-  $check->execute();
+  $result = $new_user->findByUser('id', $user);
 
-  if($check->rowCount() > 0) {
+  if($result) {
     echo json_encode('Username exists');
   } else {
     echo json_encode('true');
